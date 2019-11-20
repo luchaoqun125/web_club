967bedb1dd7e3a4558307dc7f56539df87a5a490
鲁超群 <lqc.@gegejia.com>
2019-11-20 17:53:40 +0800

    fix:追踪

--- react_webpack/src/App.js
+++ react_webpack/src/App.js
@@ -3,7 +3,7 @@
  * @Author: 鲁大师
  * @Date: 2019-11-17 22:45:14
  * @LastEditors: 鲁大师
- * @LastEditTime: 2019-11-20 16:49:35
+ * @LastEditTime: 2019-11-20 17:53:20
  */
 import React from 'react';
 import {hot} from 'react-hot-loader/root';
@@ -15,7 +15,7 @@ import { Button } from 'antd'
 function App(){
     return (
         <div className={styles.text}>
-            hello reactffff
+            hello reactfffsssf
             <img src={img} alt=""/>
             <img src={img1} alt=""/>
             <Button type="danger">click me2</Button>
